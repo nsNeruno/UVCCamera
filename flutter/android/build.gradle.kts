a4be8f198796d2df21ed25b7128606780563b88b
Nanan Setiady <infinia249@gmail.com>
2025-11-13 09:01:42 +0700

    [Gradle] Hardcoded UVC Lib version

--- flutter/android/build.gradle.kts
+++ flutter/android/build.gradle.kts
@@ -2,12 +2,12 @@ plugins {
     id("com.android.library")
 }
 
-version = run {
-    val pubspecContent = file("../pubspec.yaml").readText()
-    val pubspecVersionMatch = Regex("version:\\s+(.*)").find(pubspecContent)
-    val pubspecVersion = pubspecVersionMatch?.groupValues?.get(1)
-    pubspecVersion ?: "0.0.0-SNAPSHOT"
-}
+//version = run {
+//    val pubspecContent = file("../pubspec.yaml").readText()
+//    val pubspecVersionMatch = Regex("version:\\s+(.*)").find(pubspecContent)
+//    val pubspecVersion = pubspecVersionMatch?.groupValues?.get(1)
+//    pubspecVersion ?: "0.0.0-SNAPSHOT"
+//}
 
 android {
     namespace = "org.uvccamera.flutter"
@@ -26,7 +26,7 @@ android {
 
 dependencies {
     implementation("androidx.annotation:annotation:1.9.1")
-    implementation("org.uvccamera:lib:$version") // NOTE: Use org.uvccamera:lib of the same version as the plugin.
+    implementation("org.uvccamera:lib:0.0.13") // NOTE: Use org.uvccamera:lib of the same version as the plugin.
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.mockito:mockito-core:5.0.0")
 }
